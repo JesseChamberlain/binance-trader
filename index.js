5eb2c76649aa43eb1d200bf5f338ab78db5b4be3
Jesse Chamberlain <jessechamberlain@admins-Mac-Pro.local>
2021-02-22 18:31:50 -0500

    working buy/sell function

--- index.js
+++ index.js
@@ -1,164 +1,60 @@
+require('dotenv').config();
+const ccxt = require('ccxt');
 const axios = require('axios');
 const { createCoinGeckoURL } = require('./helpers/coinGeckoURL');
-const fs = require('fs');
 
-// CoinData object for storing and mutating current stage of coin price
-class CoinData {
-    constructor(coinID, currency) {
-        this.coinID = coinID;
-        this.currency = currency;
-        this.currentPrice = 0;
-        this.previousPrice = 0;
-        this.currentTrendUp = true;
-        this.previousTrendUp = true;
-    }
-}
+const tick = async (config, binanceClient) => {
+    const { asset, assetID, base, baseID, currency } = config;
+    const marketSymbol = `${asset}/${base}`;
 
-// Account object for storing and mutating account values
-class Account {
-    constructor(startingValue, binanceFee) {
-        this.startingValue = startingValue;
-        this.theoryValue = startingValue;
-        this.binanceFee = binanceFee;
-    }
-}
+    // Create API urls for price lookup
+    const assetURL = createCoinGeckoURL(assetID, currency);
+    const baseURL = createCoinGeckoURL(baseID, currency);
 
-// Creates the file that the data will be stored to
-function createDataCollectionJSON(testCoinData) {
-    const time = new Date();
-    const year = time.getFullYear();
-    const day = time.getDate();
-    const month = time.getMonth() + 1; // stupid base zero month
-    let filePath = `./data/${year}_${month}_${day}_${testCoinData.coinID}_${testCoinData.currency}.json`;
+    // Fetch current market prices
+    const results = await Promise.all([
+        axios.get(assetURL),
+        axios.get(baseURL),
+    ]);
+    const assetPrice = results[0].data.litecoin.usd;
+    const basePrice = results[1].data.tether.usd;
 
-    // create the JSON file with an empty array
-    fs.writeFile(filePath, JSON.stringify([]), 'utf8', (err) => {
-        if (err) {
-            console.log('Error writing file', err);
-        } else {
-            console.log('Successfully wrote createDataCollectionJSON file');
-        }
-    });
-
-    return filePath;
-}
-
-// Opens /data/collector.json (array json) and adds the response data to the array.
-function dataCollector(account, testCoinData, filePath) {
-    const time = new Date();
-    const timeToString = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
-    let ping = { time: timeToString, account, testCoinData };
-    console.log(ping);
-
-    fs.readFile(filePath, 'utf8', function readFileCallback(err, data) {
-        if (err) {
-            console.log(err);
-        } else {
-            let collectorArray = JSON.parse(data); // renders file to object
-            collectorArray.push(ping); // add the response data
-            let jsonData = JSON.stringify(collectorArray); //convert it back to json
-
-            // writes back to the file
-            fs.writeFile(filePath, jsonData, 'utf8', (err) => {
-                if (err) {
-                    console.log('Error writing file', err);
-                } else {
-                    console.log('Successfully wrote file');
-                }
-            });
-        }
-    });
-}
-
-/**
- * Runs data through logic and logs to console
- * @param {object} data - JSON object from API call.
- * @param {number} startAccountValue - Initial account value.
- */
-function factorVolatility(account, coinData) {
-    console.log(`
-        Checking Current Price
-        ****************************
-    `);
-
-    // only factor if the current price is different from previous
-    if (coinData.currentPrice !== coinData.previousPrice) {
-        let valPrcntModifier = coinData.currentPrice / coinData.previousPrice;
-        console.log('valMod:', valPrcntModifier);
-        coinData.currentTrendUp =
-            coinData.currentPrice > coinData.previousPrice ? true : false;
-
-        // primary logic
-        if (coinData.currentTrendUp && coinData.previousTrendUp) {
-            // multiply theoryVal by modifier to mimic held value
-            account.theoryValue = account.theoryValue * valPrcntModifier;
-        } else if (!coinData.currentTrendUp && coinData.previousTrendUp) {
-            // multiple by modifier and binance fee to mimic market sell
-            account.theoryValue =
-                account.theoryValue * valPrcntModifier * account.binanceFee;
-            coinData.previousTrendUp = false;
-        } else if (coinData.currentTrendUp && !coinData.previousTrendUp) {
-            // mimics buy in and resets previousTrendUp to true
-            coinData.previousTrendUp = true;
-        }
-    }
-
-    // set the previous to current at end of each priceObj loop
-    coinData.previousPrice = coinData.currentPrice;
-}
-
-// Initialize coinData
-const initialize = async (assetURL, testCoinData) => {
-    // request price from API
-    const responseData = await axios.get(assetURL).then((response) => {
-        return response.data;
-    });
-
-    // initialize previous price with live price
-    testCoinData.previousPrice =
-        responseData[testCoinData.coinID][testCoinData.currency];
+    // Sell order
+    const sellOrder = await binanceClient.createOrder(
+        marketSymbol,
+        'market',
+        'sell',
+        1,
+        assetPrice
+    );
+    // Sell order
+    const buyOrder = await binanceClient.createOrder(
+        marketSymbol,
+        'market',
+        'buy',
+        1,
+        assetPrice
+    );
+    console.log(sellOrder);
+    console.log(buyOrder);
+    console.log(assetPrice);
+    console.log(basePrice);
 };
 
-// Interval function
-const tick = async (assetURL, testCoinData, account, dataFilePath) => {
-    // request price from API
-    const responseData = await axios.get(assetURL).then((response) => {
-        return response.data;
-    });
-
-    testCoinData.currentPrice =
-        responseData[testCoinData.coinID][testCoinData.currency];
-
-    // Initialize runners
-    factorVolatility(account, testCoinData);
-
-    // Opens /data/collector.json (array json) and adds the data response to the array.
-    dataCollector(account, testCoinData, dataFilePath);
-};
-
-// Primary runner
 const run = () => {
     const config = {
-        assetID: 'dogecoin', // Coin ID
+        asset: 'LTC', // LiteCoin
+        assetID: 'litecoin', // LiteCoin ID
+        base: 'USDT', // Tether USD coin
+        baseID: 'tether', // Tether ID
         currency: 'usd', // Currency for comparison
-        startingBalance: 100,
-        binanceFee: 0.99925,
-        tickInterval: 3000, // Duration between each tick, milliseconds
     };
-    const assetURL = createCoinGeckoURL(config.assetID, config.currency);
-    let account = new Account(config.startingBalance, config.binanceFee); // end $ value of account after running through data
-    let testCoinData = new CoinData(config.assetID, config.currency);
-    let dataFilePath = createDataCollectionJSON(testCoinData);
-
-    initialize(assetURL, testCoinData);
-    setInterval(
-        tick,
-        config.tickInterval,
-        assetURL,
-        testCoinData,
-        account,
-        dataFilePath
-    );
+    // Instantiate binance client using the US binance API
+    const binanceClient = new ccxt.binanceus({
+        apiKey: process.env.API_KEY,
+        secret: process.env.API_SECRET,
+    });
+    tick(config, binanceClient);
 };
 
 run();
