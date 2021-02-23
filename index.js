a27e2ea9f49266d3e0b1b52e18ad35c85f9ab17c
Jesse Chamberlain <jessechamberlain@admins-Mac-Pro.local>
2021-02-22 20:21:23 -0500

    logging ticker and orders

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 require('dotenv').config();
 const ccxt = require('ccxt');
-const axios = require('axios');
-const { createCoinGeckoURL } = require('./helpers/coinGeckoURL');
 
 const tick = async (config, binanceClient) => {
-    const { asset, assetID, base, baseID, currency } = config;
+    const { asset, base } = config;
     const marketSymbol = `${asset}/${base}`;
-
-    // Create API urls for price lookup
-    const assetURL = createCoinGeckoURL(assetID, currency);
-    const baseURL = createCoinGeckoURL(baseID, currency);
-
-    // Fetch current market prices
-    const results = await Promise.all([
-        axios.get(assetURL),
-        axios.get(baseURL),
-    ]);
-    const assetPrice = results[0].data.litecoin.usd;
-    const basePrice = results[1].data.tether.usd;
     const binanceTicker = await binanceClient.fetchTicker(marketSymbol);
 
     // Sell order
@@ -26,30 +12,25 @@ const tick = async (config, binanceClient) => {
         'market',
         'sell',
         1,
-        assetPrice
+        binanceTicker
     );
-    // Sell order
+    // buy order
     const buyOrder = await binanceClient.createOrder(
         marketSymbol,
         'market',
         'buy',
         1,
-        assetPrice
+        binanceTicker
     );
+    console.log(binanceTicker.last);
     console.log(sellOrder);
     console.log(buyOrder);
-    console.log(assetPrice);
-    console.log(basePrice);
-    console.log(binanceTicker);
 };
 
 const run = () => {
     const config = {
         asset: 'LTC', // LiteCoin
-        assetID: 'litecoin', // LiteCoin ID
         base: 'USDT', // Tether USD coin
-        baseID: 'tether', // Tether ID
-        currency: 'usd', // Currency for comparison
     };
     // Instantiate binance client using the US binance API
     const binanceClient = new ccxt.binanceus({
@@ -57,6 +38,7 @@ const run = () => {
         secret: process.env.API_SECRET,
     });
     tick(config, binanceClient);
+    setInterval(tick, 5000, config, binanceClient);
 };
 
 run();
